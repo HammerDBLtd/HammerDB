d05435d90be201c2f6d7824a68e9e7ead12ebc96
Steve Shaw <38044085+sm-shaw@users.noreply.github.com>
2026-09-15 09:18:07 +0100

    Keep web service SSL override wiring scoped

--- src/generic/geninitws.tcl
+++ src/generic/geninitws.tcl
@@ -52,7 +52,7 @@ if { $genericdictdb eq "" } {
                     puts "Error deleting SQLite file from $sqlite_hdb_version: $message"
             }
         }
-        #After remove old SQLite, save genericdict from XML to SQLite DB
+        #After remove old SQLite, save genericdict to SQLite DB
         Dict2SQLite "generic" $genericdict
     } else {
         #Use configration from SQLite
